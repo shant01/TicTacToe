b432f1d7e13d993720f6842f965a984bea173721
shant01 <shanthovagimian@gmail.com>
2021-09-15 18:29:48 -0700

    Basic UI layout for tic tac toe grid

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+let columns: [GridItem] = [GridItem(.flexible()),
+                           GridItem(.flexible()),
+                           GridItem(.flexible())]
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 5) {
+                    ForEach(0..<9) {i in
+                        ZStack{
+                            Circle()
+                                .foregroundColor(.yellow).opacity(0.5)
+                                .frame(width: geometry.size.width/3 - 25,
+                                       height: geometry.size.width/3 - 25)
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .foregroundColor(.white)
+                                .frame(width: 40, height: 40)
+                                
+                            
+                            
+                        }
+                    }
+                    
+                }
+                Spacer()
+            }
+            
+        }
+        
+        
     }
 }
 
